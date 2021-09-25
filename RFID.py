2545bc82742fc739d065554d03dfa2141bbc5f1a
Abram <58718191+heyuhowudoin@users.noreply.github.com>
2021-09-24 17:02:55 -0700

    yes

--- RFID.py
+++ RFID.py
@@ -1,11 +1,23 @@
+import datetime
+
 input_ = ""
+present = {}
 
 def get_input():
 	input_ = input("Hold your card up")
-	if (input_ == "0017129476"):
-		print("Hello Abram! You have been marked present.")
-	elif (input_ == "0029450244"):
-		print("Hello Mr. Tessmer! You have been marked present.")
+	with open('data.json') as data_file:
+		data = json.load(data_file)
+		with open('present_today.json') as data_file:
+			present = json.load(data_file)
+
+			for ID in data:
+				if (input_ == ID):
+					if (str(data[ID][1] not in present)):
+						print("Welcome to class, " + str(data[ID][1]))
+						present[str(data[ID][1])] = datetime.datetime.now()
+
+	with open("present_today.json", "w") as data_file:
+		json.dump(present, data_file, indent = 3)
 
 while True:
 	get_input()
